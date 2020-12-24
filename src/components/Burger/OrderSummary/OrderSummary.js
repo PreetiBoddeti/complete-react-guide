240ec05d0e988815a07a0accda0401261ed469ed
PreetiBoddeti <iteerp999@gmail.com>
2020-12-24 17:46:02 +0530

    Toolbar, SideDrawer, toggle,

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,27 +1,31 @@
-import React from "react";
+import React, {Component} from "react";
 import Aux from "../../../hoc/Auxiliary";
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
-    return (
-      <li>
-        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
-        {props.ingredients[igKey]}
-      </li>
-    );
-  });
-
-  return (
-    <Aux>
+class OrderSummary extends Component {
+  componentWillUpdate(){
+    console.log('[OrderSumamry] WillUpdate');
+  }
+  render(){
+    const ingredientSummary = Object.keys(this.props.ingredients).map((igKey) => {
+      return (
+        <li>
+          <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
+          {this.props.ingredients[igKey]}
+        </li>
+      );
+    });
+    return(
+      <Aux>
       <h3>Your Order</h3>
       <p>A delicious burger with the following ingredents:</p>
       <ul>{ingredientSummary}</ul>
       <p>Continue to Checkout?</p>
-      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-      <button btntype="Danger" onClick={props.purchaseCancelled}>CANCEL</button>
-      <button btntype="Success" onClick={props.purchaseContinued}>CONTINUE</button>
+      <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+      <button btntype="Danger" onClick={this.props.purchaseCancelled}>CANCEL</button>
+      <button btntype="Success" onClick={this.props.purchaseContinued}>CONTINUE</button>
     </Aux>
-  );
-};
-
-export default orderSummary;
+    );
+ }
+}
+  
+export default OrderSummary;
